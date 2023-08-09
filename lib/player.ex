944db85dfa1953a552d48c0a85b154dc8c79a903
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-09 20:50:48 -0300

    feat: player module

---
+++ lib/player.ex
@@ -0,0 +1,18 @@
+defmodule ExMon.Player do
+    @required_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+
+    @enforce_keys  @required_keys
+    defstruct @required_keys
+
+    @max_life 100
+
+    def build(name, move_rnd,  move_avg, move_heal) do
+      %ExMon.Player{
+        name: name,
+        move_rnd: move_rnd,
+        move_avg: move_avg,
+        move_heal: move_heal,
+        life: @max_life
+      }
+    end
+end
