e844cf9e723ba6a22f451bcccd528fd3ec48c500
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-11 09:36:57 -0300

    feat: game agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,12 @@
 defmodule ExMon do
+  alias ExMon.Player
+
   def create_player(name, move_rnd, move_avg, move_heal) do
-    ExMon.Player.build(name, move_rnd, move_avg, move_heal)
+    Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    computer = create_player("Robotinik", :punch, :kick, :heal)
+    ExMon.Game.start(computer, player)
   end
 end
