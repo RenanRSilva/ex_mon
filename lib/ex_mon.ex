1e0c6ae8dc75483786123aff9e3ea7fea642d304
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-11 10:38:47 -0300

    feat: status module

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,12 +1,18 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @computer_name "Robotinik"
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
 
   def start_game(player) do
-    computer = create_player("Robotinik", :punch, :kick, :heal)
-    ExMon.Game.start(computer, player)
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
