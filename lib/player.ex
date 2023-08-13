fb2dddc727171b46146ea20c91c3aea93d783dd9
RenanRSilva <renan.rambul@takeandgoapp.com>
2023-08-13 01:42:37 -0300

    chore: format

--- lib/player.ex
+++ lib/player.ex
@@ -1,20 +1,20 @@
 defmodule ExMon.Player do
-    @required_keys [:life, :name, :moves]
+  @required_keys [:life, :name, :moves]
 
-    @enforce_keys  @required_keys
-    defstruct @required_keys
+  @enforce_keys @required_keys
+  defstruct @required_keys
 
-    @max_life 100
+  @max_life 100
 
-    def build(name, move_rnd,  move_avg, move_heal) do
-      %ExMon.Player{
-        life: @max_life,
-        moves: %{
-          move_rnd: move_rnd,
-          move_avg: move_avg,
-          move_heal: move_heal,
-        },
-        name: name,
-      }
-    end
+  def build(name, move_rnd, move_avg, move_heal) do
+    %ExMon.Player{
+      life: @max_life,
+      moves: %{
+        move_rnd: move_rnd,
+        move_avg: move_avg,
+        move_heal: move_heal
+      },
+      name: name
+    }
+  end
 end
